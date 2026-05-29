17531e008c10d4854e8f631bfc754076e0566592
MafiaBoss <kafkatokalki@gmail.com>
2026-05-29 20:49:06 +0530

    initial deployment conditions fulfilled

--- database/db_insert.py
+++ database/db_insert.py
@@ -1,12 +1,33 @@
-import sqlite3
+import psycopg2
 import logging
+import os
+import streamlit as st
+from dotenv import load_dotenv
+
+load_dotenv()
 
 def data_insert(cleaned_data:list):
-    conn=sqlite3.connect("data/ipo_gmp.db")
+    try:
+        db_url=st.secrets["db_url"]
+    except:
+        db_url=os.getenv("db_url")
+    conn=psycopg2.connect(db_url)
     c=conn.cursor()
-    c.executemany("""INSERT OR IGNORE INTO 
-                  gmp_data(ipo_price,gmp_price,lot_size,estimated_listing_price,estimated_listing_percentage,total_issue_price,company_name,ipo_type,gmp_percentage,start_date,end_date,listing_date,last_update,scraped_at)
-                  VALUES(:ipo_price,:gmp_price,:lot_size,:estimated_listing_price,:estimated_listing_percentage,:total_issue_price,:company_name,:ipo_type,:gmp_percentage,:start_date,:end_date,:listing_date,:last_update,:scraped_at)""",cleaned_data)
+    c.executemany("""
+    INSERT INTO gmp_data (
+        ipo_price, gmp_price, lot_size, estimated_listing_price, 
+        estimated_listing_percentage, total_issue_price, company_name, 
+        ipo_type, gmp_percentage, start_date, end_date, listing_date, 
+        last_update, scraped_at
+    )
+    VALUES (
+        %(ipo_price)s, %(gmp_price)s, %(lot_size)s, %(estimated_listing_price)s, 
+        %(estimated_listing_percentage)s, %(total_issue_price)s, %(company_name)s, 
+        %(ipo_type)s, %(gmp_percentage)s, %(start_date)s, %(end_date)s, %(listing_date)s, 
+        %(last_update)s, %(scraped_at)s
+    )
+    ON CONFLICT (company_name, ipo_type, scraped_at) DO NOTHING;""", cleaned_data)
+
     
     logging.info(f"""Successfully added {c.rowcount} rows in the table
           from {len(cleaned_data)} entries provided""")
--- database/db_setup.py
+++ database/db_setup.py
@@ -1,17 +1,27 @@
-import sqlite3
+import psycopg2
+import os
+import streamlit as st
+from dotenv import load_dotenv
+
+load_dotenv()
+
 def setup():
-    conn=sqlite3.connect("data/ipo_gmp.db")
+    try:
+       db_url=st.secrets["db_url"]
+    except:
+       db_url=os.getenv("db_url")
+    conn=psycopg2.connect(db_url)
     c=conn.cursor()
     c.execute('''CREATE TABLE IF NOT EXISTS gmp_data(
-                                ipo_price REAL,
-                                gmp_price REAL,
+                                ipo_price FLOAT,
+                                gmp_price FLOAT,
                                 lot_size INTEGER,
-                                estimated_listing_price REAL,
-                                estimated_listing_percentage REAL,
-                                total_issue_price REAL,
+                                estimated_listing_price FLOAT,
+                                estimated_listing_percentage FLOAT,
+                                total_issue_price FLOAT,
                                 company_name TEXT,
                                 ipo_type TEXT,
-                                gmp_percentage REAL,
+                                gmp_percentage FLOAT,
                                 start_date TEXT,
                                 listing_date TEXT,
                                 last_update TEXT,
