97980fd0bb45a167a6c604f61a0df42575901131
MafiaBoss <kafkatokalki@gmail.com>
2026-06-01 19:32:01 +0530

    bhagwaan theek kardo problem plssss

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
 import pipeline
 import logging
 
-if __name__=="__main__":
-    logging.basicConfig(
-        filename="logs/pipeline.log",
-        level=logging.INFO,
-        format="%(asctime)s - %(levelname)s - %(message)s"
-    )
-    pipeline.run_pipeline()
+logging.basicConfig(
+    filename="logs/pipeline.log",
+    level=logging.INFO,
+    format="%(asctime)s - %(levelname)s - %(message)s"
+)
+pipeline.run_pipeline()
