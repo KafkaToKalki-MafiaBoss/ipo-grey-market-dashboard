b1044961bc31933cb83e788c2b239f6d482762c6
MafiaBoss <kafkatokalki@gmail.com>
2026-06-01 18:41:57 +0530

    I dont know what I solved but I solved something big and i am just happy that i debbuged in 5 minutes

--- main.py
+++ main.py
@@ -1,22 +1,10 @@
-from zoneinfo import ZoneInfo
-from apscheduler.schedulers.blocking import BlockingScheduler
-from apscheduler.triggers.cron import CronTrigger
 import pipeline
 import logging
 
-ist=ZoneInfo("Asia/Kolkata")
-
-scheduler=BlockingScheduler(timezone=ist)
-
-# trigger=CronTrigger.from_crontab("0 10-17/2,19 * * *")
-
-# scheduler.add_job(pipeline.run_pipeline,trigger=trigger)
-scheduler.add_job(pipeline.run_pipeline)
-
-logging.basicConfig(
-    filename="logs/pipeline.log",
-    level=logging.INFO,
-    format="%(asctime)s - %(levelname)s - %(message)s"
-)
-
-scheduler.start()
+if __name__=="__main__":
+    logging.basicConfig(
+        filename="logs/pipeline.log",
+        level=logging.INFO,
+        format="%(asctime)s - %(levelname)s - %(message)s"
+    )
+    pipeline.run_pipeline()
